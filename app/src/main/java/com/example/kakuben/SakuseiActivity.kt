59305832890ca1d6e3059acf0a1db370bdfedac6
KOsyuki <67924755+KOsyuki@users.noreply.github.com>
2020-08-09 03:50:13 +0900

    デモ2本完成

--- app/src/main/java/com/example/kakuben/SakuseiActivity.kt
+++ app/src/main/java/com/example/kakuben/SakuseiActivity.kt
@@ -11,7 +11,6 @@ class SakuseiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sakusei)
-        immersiveMode()
 
         //値受け渡し処理
         findViewById<View>(R.id.ok).setOnClickListener {
@@ -30,12 +29,21 @@ class SakuseiActivity : AppCompatActivity() {
         }
     }
 
-    //没入モード
-    private fun immersiveMode() {
+    //フルスクリーンモード
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUI()
+        }
+    }
+    private fun hideSystemUI() {
         val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         decorView.setOnSystemUiVisibilityChangeListener { visibility -> // Note that system bars will only be "visible" if none of the
             // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
             if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
